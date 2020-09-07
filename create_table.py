f3252bc9018b96a71859c6606e5c967057ea3ed3
Apekshaba <70003063+Apekshaba@users.noreply.github.com>
2020-09-07 12:10:06 +0530

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -7,7 +7,7 @@ cursor = connection.cursor()
 create_table = "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
 
-create_table = "CREATE TABLE IF NOT EXISTS Inventory(name text,category text, expiry_date INTEGER, quantity real, manufacturing_date INTEGER,id INTEGER PRIMARY KEY, image BLOB NOT NULL, status text)"
+create_table = "CREATE TABLE IF NOT EXISTS Inventory(name text,category text, expiry_date string, quantity real, manufacturing_date string, id INTEGER PRIMARY KEY, image BLOB NOT NULL, status text, expired boolean)"
 cursor.execute(create_table)
 
 create_table = "CREATE TABLE IF NOT EXISTS items(name text, price real)"
